e28441b9d468b9c2b82c4c9f432c11bc28656918
ChevalierAlexis <93645078+ChevalierAlexis@users.noreply.github.com>
2022-02-06 16:06:42 +0100

    Add files via upload

---
+++ ISS/assets/Back 1.tsx
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Back 1" tilewidth="32" tileheight="32" tilecount="1980" columns="60">
+ <image source="Back 1.png" width="1920" height="1080"/>
+ <tile id="0">
+  <properties>
+   <property name="estSolide" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="60">
+  <properties>
+   <property name="estSolide" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="120">
+  <properties>
+   <property name="estSolide" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="180">
+  <properties>
+   <property name="estSolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1800">
+  <properties>
+   <property name="estSolide" type="bool" value="true"/>
+  </properties>
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="0" y="12" width="32" height="20"/>
+   <object id="3" x="0" y="12" width="32" height="20"/>
+   <object id="4" x="0" y="12" width="32" height="20"/>
+  </objectgroup>
+ </tile>
+ <tile id="1860">
+  <properties>
+   <property name="estSolide" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="1920">
+  <properties>
+   <property name="estSolide" type="bool" value="false"/>
+  </properties>
+ </tile>
+</tileset>
